36e443282112d47171e3d963d715f0409460510c
Osy odife <osyodife@gmail.com>
2019-10-17 13:56:31 -0400

    changes to the book component

--- bookapp/src/App.js
+++ bookapp/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 // import FriendCard from "./components/FriendCard";
 // import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import Book from "./components/Book";
 //import friends from "./friends.json";
 class App extends Component {
 render() {
   return (
     <div className="App">
       <Header />
+      <Book />
     </div>
   );
 }
---
+++ bookapp/src/components/Book/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import FriendCard from "./components/FriendCard";
+// import Wrapper from "./components/Wrapper";
+//import friends from "./friends.json";
+class Book extends Component {
+    render() {
+        return (
+            <div >
+                
+            </div>
+        );
+    }
+}
+export default Book;
